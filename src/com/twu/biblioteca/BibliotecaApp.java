97cb090d6765074319f27a48df1c6f9ddd73c679
Vivian Liao <vivianliao@VivianLiao-Mac.local>
2019-02-04 22:15:59 -0600

    story 1.2

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,8 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public String welcome() {
+        String message = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        return  message;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,22 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+
+    public void shouldReturnWelcome() {
+        //upon starting the application, the message should return a welcome message
+
+        BibliotecaApp app = new BibliotecaApp();
+        String message = app.welcome();
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+        assertThat(message, is(expectedMessage));
     }
+
 }
