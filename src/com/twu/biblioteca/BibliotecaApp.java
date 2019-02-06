58594d4cc938c476ce69b734db7d7a31f879c54a
Vivian Liao <vivianliao@VivianLiao-Mac.local>
2019-02-05 21:26:32 -0600

    Vivian, 1.5, invalid options, control flow of Biblioteca app

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,6 +2,7 @@ package com.twu.biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
@@ -27,11 +28,31 @@ public class BibliotecaApp {
 
     public static void main (String[] args){
         BibliotecaApp app = new BibliotecaApp();
+
+        //initialize bookslist
+        List<String> books = new ArrayList<String>();
+        books.add("The Great Gatsby");
+        books.add("Ender's Game");
+
         System.out.println(app.welcome());
-       // print(app.showMenuMessage());
+        print(app.showMenuMessage());
+
+        //respond to user choice
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        switch(i){
+            case 1:
+                print(app.listBooks(books));
+                break;
+            default:
+                print("Invalid option chosen");
+        }
     }
 
     public static void print(String message){
+        System.out.println(message);
+    }
+    public static void print(List<String> message){
         System.out.println(message);
     }
 }
